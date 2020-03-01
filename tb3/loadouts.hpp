ebfb5559baad907a7fd50af7a520011e1ca69f85
Kristian Bjørge Auestad <kauestad@gmail.com>
2020-03-01 19:06:23 +0100

    Initial commit

---
+++ tb3/loadouts.hpp
@@ -0,0 +1,5 @@
+//Loadouts called with: [this,"side_class","unit_class"] call tb3_fnc_Loadout;
+//Use those bellow as an example as to creating a side and unit class.
+class TB3_Gear {
+	#include "BSO_1990s_merc_semiuniform_temperate.hpp"
+};
